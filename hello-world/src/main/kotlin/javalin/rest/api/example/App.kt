292451858830817fd5d82980c6f3fd7e2057bb4f
vn7n24fzkq <vn7n24fzkq@gmail.com>
2020-04-18 04:48:07 +0800

    Add REST API

--- hello-world/src/main/kotlin/javalin/rest/api/example/App.kt
+++ hello-world/src/main/kotlin/javalin/rest/api/example/App.kt
@@ -4,62 +4,8 @@
 package javalin.rest.api.example
 
 import io.javalin.Javalin
-import io.javalin.apibuilder.ApiBuilder.*
-import io.javalin.http.Context
-
-data class User(
-    var id:Int,
-    var name:String
-)
 
 fun main(args: Array<String>) {
-    val app = Javalin.create().routes{
-        path("users"){
-            get(":id",UserController::getUserById)
-            post(UserController::createUser)
-            put(UserController::updateUser)
-            delete(":id",UserController::removeUserById)
-        }
-    }.start(7000)
+    val app = Javalin.create().start(7000)
     app.get("/") { ctx -> ctx.result("Hello World") }
 }
-
-
-val users = HashMap<Int,User>()
-
-object UserController{
-
-    fun createUser(ctx:Context){
-        ctx.bodyValidator<User>().get().apply{ 
-            if(!users.containsKey(this.id))
-                users.put(this.id,this)
-            else
-                ctx.status(400)
-        }
-    }
-    
-    fun getUserById(ctx:Context){
-        val user = users.get(
-            ctx.pathParam("id").toInt()
-        )
-        user?.let{
-            ctx.json(it)
-        }?:ctx.status(400)
-    }
-
-    fun removeUserById(ctx:Context){
-        val id = ctx.pathParam("id").toInt()
-        users.remove(id)
-    }
-
-    fun updateUser(ctx:Context){
-        ctx.bodyValidator<User>().get().apply{ 
-            if(users.containsKey(this.id))
-                users.put(this.id,this)
-            else
-                ctx.status(400)
-        }
-    }
-
-}
-
